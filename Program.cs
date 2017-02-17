07536caa8e00b16bf04a846514d4365381ad7451
DESKTOP-3SNT22J\fruzsinakarman <karman.fruzsina@gmail.com>
2017-02-17 10:55:49 +0100

    numbersums of rows in lists

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                RowChecker strhandler = new RowChecker();
+                string str = "Greenfox";
+                Console.WriteLine(strhandler.Reverse(str));
+
+                
+            }
+
+        }
+    }
+}
---
+++ RowChecker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExam
+{
+    class RowChecker
+    {
+        public string Reverse(string text)
+        {
+            try
+            {
+                string reverse = "";
+                for (int i = text.Length - 1; i >= 0; i--)
+                {
+                    reverse += text[i];
+                }
+                return reverse;
+            }
+            catch (Exception e)
+            {
+                return "An error occurred: '{0}'" + e;
+            }
+        }
+    }
+}
